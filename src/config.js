2a69f3a9eac8f920ec3234972fe7b89f58f678a1
Yavuz <yavuz@Yavuz-MBP.local>
2023-01-09 19:49:09 +0100

    baseURL changed

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({ // bunu surekli ayni seyi yazmamak icin yazdim
     //baseURL: 'http://localhost:8080',
-    baseURL: 'https://reactredux-todoapp-withauth-be.herokuapp.com/',
+    //baseURL: 'https://reactredux-todoapp-withauth-be.herokuapp.com/',
+    baseURL: 'https://reactredux-todoapp-withauth-be.onrender.com/',
     // withCredentials: true, // cookie icin
 })
